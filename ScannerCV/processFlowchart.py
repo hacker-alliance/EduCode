f9caa4a6478ff586884f4bce5a74e1684abe8f70
Blake Wyatt <nabakin@protonmail.ch>
2019-10-27 09:58:59 -0400

    Add automatic corner finder, Google Vision API text identification, and intermediate language output

--- ScannerCV/processFlowchart.py
+++ ScannerCV/processFlowchart.py
@@ -1,6 +1,7 @@
 import argparse
 import imutils
 import cv2
+import json
 import numpy as np
 
 # construct the argument parse and parse the arguments
@@ -57,12 +58,39 @@ cv2.circle(cornerImage, tuple(approx[1][0]), 50, (57, 255, 20), -1)
 cv2.circle(cornerImage, tuple(approx[2][0]), 50, (57, 255, 20), -1)
 cv2.circle(cornerImage, tuple(approx[3][0]), 50, (57, 255, 20), -1)
 
+
 cv2.imwrite('corners.png', cornerImage)
 
 h, w, _ = image.shape
 approx = np.array([approx[0][0], approx[3][0], approx[2][0], approx[1][0]], dtype="float32")
+temp = np.empty((4, 2), dtype="float32")
+for point in approx:
+    countx1 = 0
+    countx2 = 0
+    county1 = 0
+    county2 = 0
+    for check in approx:
+        if check[0] != point[0] or check[1] != point[1]:
+            if point[0] <= check[0]:
+                countx1 = countx1 + 1
+            else:
+                countx2 = countx2 + 1
+            if point[1] <= check[1]:
+                if point[0] == 2864 and point[1] == 3771:
+                    print(point, check)
+                county1 = county1 + 1
+            else:
+                county2 = county2 + 1
+    if countx1 > 1 and county1 > 1:
+        temp[0] = point
+    if countx2 > 1 and county1 > 1:
+        temp[1] = point
+    if countx1 > 1 and county2 > 1:
+        temp[3] = point
+    if countx2 > 1 and county2 > 1:
+        temp[2] = point
 
-(tl, tr, br, bl) = approx
+(tl, tr, br, bl) = temp
 
 # compute the width of the new image, which will be the
 # maximum distance between bottom-right and bottom-left
@@ -79,11 +107,11 @@ heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
 maxHeight = max(int(heightA), int(heightB))
 
 transformCoords = np.array([[0, 0], [maxWidth-1, 0], [maxWidth-1, maxHeight-1], [0, maxHeight-1]], dtype="float32")
-transform = cv2.getPerspectiveTransform(approx, transformCoords)
+transform = cv2.getPerspectiveTransform(temp, transformCoords)
 perspective = cv2.warpPerspective(image.copy(), transform, (maxWidth, maxHeight))
 h, w, _ = perspective.shape
 
-outterCropProportion = 0.1
+outterCropProportion = 0
 widthCut = int(maxWidth*outterCropProportion/2)
 heightCut = int(maxHeight*outterCropProportion/2)
 perspectiveImage = perspective[heightCut:h-1-heightCut, widthCut:w-1-widthCut]
@@ -147,6 +175,8 @@ cntsA=[cntsA[i] for i in WithoutChildContour]
 ratio = perspectiveImage.shape[0] / float(perspectiveImage.shape[0])
 shapeContoursImage = perspectiveImage.copy()
 
+intermediateJSON = []
+i = 0
 # loop over the contours
 for c in cntsA:
     # compute the center of the contour, then detect the name of the
@@ -156,6 +186,8 @@ for c in cntsA:
     cX = int((M["m10"] / (M["m00"]+1)) * ratio)
     cY = int((M["m01"] / (M["m00"]+1)) * ratio)
     shape = detect(c)
+    intermediateJSON.append({"shape": shape, "value": ''})
+    i = i+1
 
     # multiply the contour (x, y)-coordinates by the resize ratio,
     # then draw the contours and the name of the shape on the image
@@ -163,8 +195,68 @@ for c in cntsA:
     c *= ratio
     c = c.astype("int")
     cv2.drawContours(shapeContoursImage, [c], -1, (57, 255, 20), 2)
-    cv2.putText(shapeContoursImage, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    cv2.putText(shapeContoursImage, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), 2)
 
 #shapeContoursImage = cv2.drawContours(perspectiveImage.copy(), cntsA, -1, (57, 255, 20), 2)
 
 cv2.imwrite('shapecontours.png', shapeContoursImage)
+
+def detect_document(path):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+
+    if len(response.text_annotations) > 0:
+        return response.text_annotations[0].description
+    else:
+        return ''
+    """
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
+
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    print('Word text: {} (confidence: {})'.format(
+                        word_text, word.confidence))
+
+                    for symbol in word.symbols:
+                        print('\tSymbol: {} (confidence: {})'.format(
+                            symbol.text, symbol.confidence))
+
+    """
+
+
+i = 0
+for c in cntsA:
+    x,y,width,height = cv2.boundingRect(c)
+    out = imagePGray[y:y+height, x:x+width]
+    cv2.imwrite('temp.png', out, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+    intermediateJSON[i]['value'] = detect_document('temp.png')
+    i = i+1
+
+intermediateJSONFin = []
+for i in range(len(intermediateJSON)):
+    if intermediateJSON[i]['value'] != '':
+        intermediateJSONFin.append(intermediateJSON[i])
+
+# Reversing a list using reversed()
+def Reverse(lst):
+    return [ele for ele in reversed(lst)]
+
+print(Reverse(intermediateJSONFin))
