8df221e318e37c868f95b6e652f4eee2ad8f787b
Blake Wyatt <nabakin@protonmail.ch>
2019-10-27 05:15:47 -0400

    Add perspective transform

--- ScannerCV/processFlowchart.py
+++ ScannerCV/processFlowchart.py
@@ -21,10 +21,10 @@ thresh = cv2.threshold(edges.copy(), 80, 160, cv2.THRESH_BINARY)[1]
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
 dilated = cv2.dilate(thresh.copy(), kernel)
+cv2.imwrite('canny.png', dilated)
 
 # Obtain contours, looking for scantron outline
 contours, _ = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.imwrite('canny.png', dilated)
 
 mx = (0,0,0,0)      # biggest bounding box so far
 mx_area = 0
@@ -59,4 +59,33 @@ cv2.circle(cornerImage, tuple(approx[3][0]), 50, (57, 255, 20), -1)
 
 cv2.imwrite('corners.png', cornerImage)
 
-cv2.getPerspectiveTransform(approx, )
+h, w, _ = image.shape
+approx = np.array([approx[0][0], approx[3][0], approx[2][0], approx[1][0]], dtype="float32")
+
+(tl, tr, br, bl) = approx
+
+# compute the width of the new image, which will be the
+# maximum distance between bottom-right and bottom-left
+# x-coordiates or the top-right and top-left x-coordinates
+widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+maxWidth = max(int(widthA), int(widthB))
+
+# compute the height of the new image, which will be the
+# maximum distance between the top-right and bottom-right
+# y-coordinates or the top-left and bottom-left y-coordinates
+heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+maxHeight = max(int(heightA), int(heightB))
+
+transformCoords = np.array([[0, 0], [maxWidth-1, 0], [maxWidth-1, maxHeight-1], [0, maxHeight-1]], dtype="float32")
+transform = cv2.getPerspectiveTransform(approx, transformCoords)
+perspective = cv2.warpPerspective(image.copy(), transform, (maxWidth, maxHeight))
+h, w, _ = perspective.shape
+
+outterCropProportion = 0.1
+widthCut = int(maxWidth*outterCropProportion/2)
+heightCut = int(maxHeight*outterCropProportion/2)
+perspectiveImage = perspective[heightCut:h-1-heightCut, widthCut:w-1-widthCut]
+
+cv2.imwrite('perspective.png', perspectiveImage)
